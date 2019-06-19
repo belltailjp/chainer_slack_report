6488c483abc30824274ad052f428f248be5c85b3
Daichi SUZUO <belltailjp@gmail.com>
2019-06-19 18:25:35 +0900

    Hide internal variables

--- slack_report.py
+++ slack_report.py
@@ -12,39 +12,39 @@ class SlackReport(chainer.training.extensions.PrintReport):
                  label=None, log_report='LogReport'):
         super(SlackReport, self).__init__(
             entries, log_report=log_report, out=io.StringIO())
-        self.access_token = access_token
-        self.channel_id = channel_id
+        self._access_token = access_token
+        self._channel_id = channel_id
 
-        self.ts = None
+        self._ts = None
 
-        self.label = label
+        self._label = label
         if label is None:
-            self.label = " ".join(sys.argv)
+            self._label = " ".join(sys.argv)
 
     def _print(self, observation):
         super(SlackReport, self)._print(observation)
 
         s = self._out.getvalue().replace('\x1b[J', '')  # Remove clear screen
         s = "```{}```".format(s)
-        if self.label:
-            s = "{}\n{}".format(self.label, s)
+        if self._label:
+            s = "{}\n{}".format(self._label, s)
 
         params = {
-            'token': self.access_token,
-            'channel': self.channel_id,
+            'token': self._access_token,
+            'channel': self._channel_id,
             'text': s
         }
-        if self.ts is None:
+        if self._ts is None:
             # New post
             res = requests.post('https://slack.com/api/chat.postMessage',
                                 data=params)
             res = json.loads(res.text)
             if not res['ok']:
                 return
-            self.ts = res['ts']
+            self._ts = res['ts']
         else:
             # Update the post
-            params['ts'] = self.ts
+            params['ts'] = self._ts
             res = requests.post('https://slack.com/api/chat.update',
                                 data=params)
             res = json.loads(res.text)
