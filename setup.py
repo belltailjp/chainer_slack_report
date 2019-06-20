0b2ba2e0ad59791c18aa52780f57ad28d2d0f9b3
Daichi SUZUO <belltailjp@gmail.com>
2019-06-20 11:37:49 +0900

    URL is required for pypi distribution

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 import chainer_slack_report
 
 setup(name='chainer-slack-report',
+      url='https://github.com/belltailjp/chainer_slack_report',
       version=chainer_slack_report.__version__,
       description='Send PrintReport content to Slack',
       author='Daichi SUZUO',
