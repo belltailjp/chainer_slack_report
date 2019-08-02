8265234517162ec27c0db79a78f9000ea62240bb
Daichi SUZUO <belltailjp@gmail.com>
2019-08-02 13:04:59 +0900

    Import version without importing the entire chainer_slack_report module

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import os
 from setuptools import setup
-import chainer_slack_report
+
+
+# Get __version__
+here = os.path.abspath(os.path.dirname(__file__))
+exec(open(os.path.join(here, 'chainer_slack_report', 'version.py')).read())
+
 
 setup(name='chainer-slack-report',
       url='https://github.com/belltailjp/chainer_slack_report',
-      version=chainer_slack_report.__version__,
+      version=__version__,      # NOQA
       description='Send PrintReport content to Slack',
       author='Daichi SUZUO',
       author_email='belltailjp@gmail.com',
